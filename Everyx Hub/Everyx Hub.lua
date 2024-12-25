21bd4af97a42710204eaaad87bac9246b4c5ec43
Everyx333 <quizix333@gmail.com>
2024-12-25 19:38:38 +0700

    Update Everyx Hub.lua

--- Everyx Hub/Everyx Hub.lua
+++ Everyx Hub/Everyx Hub.lua
@@ -23,7 +23,23 @@ local Tabs = {
 
 local Options = Fluent.Options
 local lp = game.Players.LocalPlayer
+local character = lp.Character or lp.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
 local RS = game.ReplicatedStorage
+local VirtualUser = game:GetService("VirtualUser")
+
+
+local function rotateToGround()
+    local position = hrp.Position
+    
+    local downwardsCFrame = CFrame.new(position, position - Vector3.new(0, 1, 0))
+    hrp.CFrame = downwardsCFrame
+end
+
+local function autoClick()
+    VirtualUser:ClickButton1(Vector2.new(0, 0))
+    wait()
+end
 
 
 local function getAccessories()
@@ -38,23 +54,90 @@ local function getAccessories()
     return accessoriesTable
 end
 
+local selectedNPC
 
-local function getNPCs()
-    local npcsFolder = Workspace.Mob
-    local npcsTable = {}
-    for _, npc in ipairs(npcsFolder:GetDescendants()) do
-        if npc:IsA("Model") then
-            if not table.find(npcsTable,npc.Name) then
-                table.insert(npcsTable,npc.Name)
-            end    
-        end 
+local function getNPC()
+    local dist, thing = math.huge
+    for i, v in pairs(game:GetService("Workspace").Mob:GetChildren()) do
+        if v.Name == selectedNPC then
+            local humanoid = v:FindFirstChild("Humanoid")
+            if humanoid and humanoid.Health > 0 then
+                local mag = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).Magnitude
+                if mag < dist then 
+                    dist = mag 
+                    thing = v 
+                end
+            end
+        end
     end
-    return npcsTable      
+    return thing
 end
+
+local function getFX()
+    local fxFolder = Workspace.FX
+    for _, effect in ipairs(fxFolder:GetDescendants()) do
+        if effect.Name ~= "cidbata2" then
+            effect:Destroy()
+        end
+    end
+
+    for _, highlight in ipairs(Workspace.Character:FindFirstChild(lp.Name):GetDescendants()) do
+        if highlight.Name == "Highlight" then
+            highlight:Destroy()
+        end
+    end
+end
+
+
+local TweenService  = game:GetService("TweenService")
+local noclipE = false
+local antifall = false
+ 
+local function noclip()
+    for i, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+        if v:IsA("BasePart") and v.CanCollide == true then
+            v.CanCollide = false
+            game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+        end
+    end
+end
+ 
+local function moveto(obj, speed)
+    local info = TweenInfo.new(((game.Players.LocalPlayer.Character.HumanoidRootPart.Position - obj.Position).Magnitude)/ speed,Enum.EasingStyle.Linear)
+    local tween = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, info, {CFrame = obj})
+ 
+    if not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyVelocity") then
+        antifall = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
+        antifall.Velocity = Vector3.new(0,0,0)
+        noclipE = game:GetService("RunService").Stepped:Connect(noclip)
+        tween:Play()
+    end
+ 
+    tween.Completed:Connect(function()
+        antifall:Destroy()
+        noclipE:Disconnect()
+    end)
+end
+
+local AFOffset = 15
+local afnpc = false
+
+local npcTable = {}
+
+function getNPCs()
+    for _, npc in ipairs(Workspace.Mob:GetDescendants()) do
+        if npc:IsA("Model") then
+            if not table.find(npcTable, npc.Name) then
+                table.insert(npcTable, npc.Name)
+            end
+        end
+    end    
+    return npcTable
+end    
 
 --auto farm tab
 
-local selectedNPC
+
 
 local npcDropdown = Tabs.AF:AddDropdown("Dropdown", {
     Title = "NPC",
@@ -67,6 +150,23 @@ npcDropdown:SetValue("Bacon")
 npcDropdown:OnChanged(function(Value)
    selectedNPC = Value
 end)
+
+local AFNPC = Tabs.AF:AddToggle("AFNPC", {Title = "Auto farm selected npc", Default = false })
+
+AFNPC:OnChanged(function()
+    afnpc = Options.AFNPC.Value
+    print(afnpc)
+    while afnpc == true do 
+        task.wait()
+        moveto(getNPC().HumanoidRootPart.CFrame + Vector3.new(0,AFOffset,0), 1000)
+        game:GetService("VirtualUser"):ClickButton1(Vector2.new(9e9, 9e9))
+    end
+end)
+
+Tabs.Main:AddParagraph({
+    Title = "Auto Farm Settings",
+    Content = "Auto Farm Settings are below this message!"
+})
 
 --misc tab
 local selectedAccessory
@@ -95,6 +195,24 @@ Tabs.Misc:AddButton({
         
     end
 })
+
+
+local FxToggle = Tabs.Misc:AddToggle("FxDestroy", {Title = "Press to destroy the fx, unrevertable", Default = false })
+
+    FxToggle:OnChanged(function()
+        Fluent:Notify({
+            Title = "Warning",
+            Content = "If you want to turn it off, disable the toggle and rejoin!",
+            SubContent = "",
+            Duration = 10
+        })
+        while OptionsFxDestroy.Value == true do
+            wait()
+            getFX()
+        end
+    end)
+
+    Options.FxDestroy:SetValue(false)
 
 
 SaveManager:SetLibrary(Fluent)
