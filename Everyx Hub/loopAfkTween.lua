e7267e504f12e7882d8a93a4c473038ab54d0173
Everyx333 <quizix333@gmail.com>
2025-08-15 18:47:10 +0700

    Create loopAfkTween.lua

---
+++ Everyx Hub/loopAfkTween.lua
@@ -0,0 +1,27 @@
+local function moveTo(obj, speed)
+    local info = TweenInfo.new((
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Position - obj.Position
+    ).Magnitude / speed, Enum.EasingStyle.Linear)
+    local tween = game.TweenService:Create(
+        game.Players.LocalPlayer.Character.HumanoidRootPart, 
+        info, 
+        {CFrame = obj.CFrame}
+    )
+
+    if not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyVelocity") then
+        antifall = Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
+        antifall.Velocity = Vector3.new(0, 0, 0)
+        noclipE = game:GetService("RunService").Stepped:Connect(noclip)
+        tween:Play()
+    end
+
+    tween.Completed:Connect(function()
+        antifall:Destroy()
+        noclipE:Disconnect()
+    end)
+end
+
+while true do
+    moveTo(workspace.Map:GetChildren()[9]:GetChildren()[6],250)
+    task.wait(1)
+end
