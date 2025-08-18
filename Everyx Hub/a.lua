a44c630783136e5b173a153dd1e630f478a36883
Everyx333 <quizix333@gmail.com>
2025-08-18 08:51:58 +0700

    Update a.lua

--- Everyx Hub/a.lua
+++ Everyx Hub/a.lua
@@ -1,15 +1,9 @@
-local VIM = game:GetService("VirtualInputManager")
-local GuiService = game:GetService("GuiService")function replicateButtonClick(button) --path
-        GuiService.SelectedObject = button
-    VIM:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-    wait(0.05)
-    VIM:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-    wait(0.05)
-    GuiService.GuiNavigationEnabled = false
-end
+
 
 while true do
     task.wait()
-    replicateButtonClick(game:GetService("Players").LocalPlayer.PlayerGui.Screen.Quests.Container.Get)
-    replicateButtonClick(game:GetService("Players").LocalPlayer.PlayerGui.Screen.Quests.Container.Frame.Claim)
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GetQuest"):InvokeServer()
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GetQuest"):InvokeServer()
+
 end
